97f9b2698e9ba2f7886ce5571e4b6ea14c6ac451
Robert Hausch <rob.hausch@gmail.com>
2017-02-26 09:59:31 -0800

    Initial commit

---
+++ js/WebGL.js
@@ -0,0 +1,21 @@
+/**
+ * Created by robha on 2017-02-26.
+ */
+/* WebGL functions from https://code.tutsplus.com/articles/webgl-essentials-part-i--net-25856 */
+
+function WebGL(CID, FSID, VSID){
+    var canvas = document.getElementById(CID);
+    if(!canvas.getContext("webgl") && !canvas.getContext("experimental-webgl"))
+        alert("Your Browser Doesn't Support WebGL");
+    else
+    {
+        this.GL = (canvas.getContext("webgl")) ? canvas.getContext("webgl") : canvas.getContext("experimental-webgl");
+
+        this.GL.clearColor(1.0, 1.0, 1.0, 1.0); // this is the color
+        this.GL.enable(this.GL.DEPTH_TEST); //Enable Depth Testing
+        this.GL.depthFunc(this.GL.LEQUAL); //Set Perspective View
+        this.AspectRatio = canvas.width / canvas.height;
+
+        //Load Shaders Here
+    }
+}
